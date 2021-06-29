498cbb23c1ceb74c5d9263f333323f995efbe67a
ProspectHub <86656750+ProspectHub@users.noreply.github.com>
2021-06-29 14:48:32 +0200

    More cosmetics

--- modules/misc/__init__.py
+++ modules/misc/__init__.py
@@ -1,6 +1,5 @@
 from discord.ext import commands
 
-
 class Misc(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -8,15 +7,12 @@ class Misc(commands.Cog):
     @commands.command()
     async def ping(self, ctx):
         """Returns the average of the discord websocket latency from the past one minute"""
-        await ctx.send(
-            f"The average discord websocket latency is: `{round(self.bot.latency * 1000, 2)}` ms."
-        )
+        await ctx.send(f"The average discord websocket latency is: `{round(self.bot.latency * 1000, 2)}` ms.", reference=ctx.message, mention_author=False)
 
     @commands.command()
     async def source(self, ctx):
         """Returns the bot's Github page"""
-        await ctx.send("<https://github.com/ProspectHub/PostnRole>")
-
+        await ctx.send("<https://github.com/ProspectHub/PostnRole>", reference=ctx.message, mention_author=False)
 
 def setup(bot):
     bot.add_cog(Misc(bot))
